80afc454d85c40fbc4405bcc7efd57a8dedab27b
Ankit Singh <2501.ankit@gmail.com>
2023-06-02 11:12:22 +0530

    add: sdk branch

--- src/test/java/runners/BstackRunner.java
+++ src/test/java/runners/BstackRunner.java
@@ -7,7 +7,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.extension.*;
-import utils.SetupLocalTesting;
 
 import java.io.FileReader;
 import java.io.UnsupportedEncodingException;
@@ -17,43 +16,9 @@ import java.util.stream.Stream;
 
 public class BstackRunner implements TestTemplateInvocationContextProvider {
     public Browser browser;
-    public String username, accessKey, wss;
+    public String wss;
     private JSONObject mainConfig;
     private JSONArray platformConfig;
-    private Map<String, Object> commonCapsConfig;
-
-
-    public BstackRunner() {
-        this.username = setupCredsAndServer().get("username");
-        this.accessKey = setupCredsAndServer().get("accesskey");
-        this.wss = setupCredsAndServer().get("wss");
-    }
-
-    public HashMap<String, String> setupCredsAndServer() {
-        try {
-            if (System.getProperty("config") != null) {
-                JSONParser parser = new JSONParser();
-                mainConfig = (JSONObject) parser
-                        .parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
-            }
-            wss = (String) mainConfig.get("wss");
-            username = System.getenv("BROWSERSTACK_USERNAME");
-            if (username == null) {
-                username = (String) mainConfig.get("userName");
-            }
-            accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-            if (accessKey == null) {
-                accessKey = (String) mainConfig.get("accessKey");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        HashMap<String, String> creds = new HashMap();
-        creds.put("username", username);
-        creds.put("accesskey", accessKey);
-        creds.put("wss", wss);
-        return creds;
-    }
 
     @Override
     public boolean supportsTestTemplate(ExtensionContext extensionContext) {
@@ -65,21 +30,14 @@ public class BstackRunner implements TestTemplateInvocationContextProvider {
         List<TestTemplateInvocationContext> desiredCapsInvocationContexts = new ArrayList<>();
 
         try {
+            JSONParser parser = new JSONParser();
+            mainConfig = (JSONObject) parser
+                        .parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
             platformConfig = (JSONArray) mainConfig.get("environments");
-            commonCapsConfig = (Map<String, Object>) mainConfig.get("capabilities");
+            wss = (String) mainConfig.get("wss");
 
             for (int i = 0; i < platformConfig.size(); i++) {
                 JSONObject platform = (JSONObject) platformConfig.get(i);
-                platform.putAll(commonCapsConfig);
-                platform.put("browserstack.username", username);
-                platform.put("browserstack.accessKey", accessKey);
-                if(Objects.equals(System.getProperty("local"), "true")) {
-                    platform.put("browserstack.local", "true");
-                    HashMap<String, String> localOptions = new HashMap<>();
-                    localOptions.put("key", accessKey);
-                    //Add more local options here, e.g. forceLocal, localIdentifier, etc.
-                    SetupLocalTesting.createInstance(localOptions);
-                }
                 desiredCapsInvocationContexts.add(invocationContext(platform));
             }
         } catch (Exception e) {
@@ -104,10 +62,6 @@ public class BstackRunner implements TestTemplateInvocationContextProvider {
                     @Override
                     public Object resolveParameter(ParameterContext parameterContext,
                                                    ExtensionContext extensionContext) {
-                        String testName = extensionContext.getTestMethod().get().getName();
-                        testName += " [" + capabilitiesObject.get("os") + " " + capabilitiesObject.get("os_version");
-                        testName += " - " + capabilitiesObject.get("browser") + " " + capabilitiesObject.get("browser_version") + "]";
-                        capabilitiesObject.put("name", testName);
                         Playwright playwright = Playwright.create();
                         BrowserType browserType = playwright.chromium();
                         String caps = null;
--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -3,6 +3,7 @@ package tests;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import runners.PlaywrightTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
 
@@ -14,15 +15,10 @@ public class SampleTest {
             String product_name = page.locator("//*[@id='1']/p").textContent();
             page.locator("//*[@id='1']/div[4]").click();
             page.locator(".float\\-cart__content");
-            String product_in_cart = page.locator("//*[@id='__next']/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]").textContent();
-            // Update Test Status
-            if (product_name.equals(product_in_cart)) {
-                page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"" + "passed" + "\", \"reason\": \"" + "Product has been successfully added to the cart!" + "\"}}");
-            } else {
-                page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"" + "failed" + "\", \"reason\": \"" + "There was some issue!" + "\"}}");
-            }
+            String product_in_cart = page.locator("//*[@id='__next']/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")
+                    .textContent();
+            assertEquals(product_in_cart, product_name);
         } catch (Exception e) {
-            page.evaluate("_ => {}", "browserstack_executor: { \"action\": \"setSessionStatus\", \"arguments\": { \"status\": \"" + "failed" + "\", \"reason\": \"" + "There was some issue!" + "\"}}");
             System.out.println("Exception: " + e.getMessage());
         }
         browser.close();
